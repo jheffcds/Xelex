70e221f3042682a19b70fd4b4baccbe8856ed7f1
jheffcds <jheffcds@gmail.com>
2024-09-14 23:03:28 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,53 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const galleryImage = document.getElementById('gallery-image');
-    const galleryText = document.getElementById('gallery-text');
-    const prevButton = document.getElementById('prev-button');
-    const nextButton = document.getElementById('next-button');
-    let galleryItems = [];
-    let currentIndex = 0;
-    const toggleButton = document.getElementById('toggle-button');
-    const menu = document.getElementById('menu');
-    const dropdownLinks = document.querySelectorAll('.dropdown > a');
-    
-    toggleButton.addEventListener('click', () => {
-        menu.classList.toggle('show');
+/* ------- menu --------- */
+const menu = document.querySelector(".menu");
+const openMenuBtn = document.querySelector(".open-menu-btn");
+const closeMenuBtn = document.querySelector(".close-menu-btn");
+
+[openMenuBtn, closeMenuBtn].forEach((btn) => {
+    btn.addEventListener("click", () => {
+        menu.classList.toggle("open");
+        menu.style.transition = "trasform 0.5s ease";
     });
+});
 
-    dropdownLinks.forEach(link => {
-        link.addEventListener('click', (event) => {
-            event.preventDefault();
-            const dropdownContent = link.nextElementSibling;
-            dropdownContent.classList.toggle('show');
-        });
-    });
+menu.addEventListener("transitioned", function(){
+    this.removeAttribute("style");
+});
 
-    // Fetch gallery data from JSON
-    fetch('assets/data/gallery.json')
-        .then(response => response.json())
-        .then(data => {
-            galleryItems = data;
-            updateGallery();
-        })
-        .catch(error => console.error('Error fetching gallery data:', error));
-
-    // Update gallery content
-    function updateGallery() {
-        if (galleryItems.length > 0) {
-            const currentItem = galleryItems[currentIndex];
-            galleryImage.src = currentItem.image;
-            galleryText.textContent = currentItem.text;
-        }
-    }
-
-    // Show previous item
-    prevButton.addEventListener('click', () => {
-        currentIndex = (currentIndex === 0) ? galleryItems.length - 1 : currentIndex - 1;
-        updateGallery();
-    });
-
-    // Show next item
-    nextButton.addEventListener('click', () => {
-        currentIndex = (currentIndex === galleryItems.length - 1) ? 0 : currentIndex + 1;
-        updateGallery();
+menu.querySelectorAll(".dropdown > i").forEach((arrow) => {
+    arrow.addEventListener("click", function() {
+        this.closest(".dropdown").classList.toggle("active")
     });
 });
