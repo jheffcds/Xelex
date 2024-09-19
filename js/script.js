ef8f0fabc31395efca5034d95dcb5c0054d4e4dc
jheffcds <jheffcds@gmail.com>
2024-09-19 11:11:03 +0200

    update search field

--- js/script.js
+++ js/script.js
@@ -19,6 +19,45 @@ menu.querySelectorAll(".dropdown > i").forEach((arrow) => {
         this.closest(".dropdown").classList.toggle("active")
     });
 });
+
+// Select the search button and container where the input will be added
+const searchButton = document.querySelector('.search-btn');
+const searchContainer = document.querySelector('.search-container');
+let inputContainer; // To keep reference of inputContainer
+
+// Add event listener to search button
+searchButton.addEventListener('click', function(event) {
+    event.stopPropagation(); // Prevent this event from closing the input field
+
+    // Check if the input field already exists
+    if (!document.querySelector('.sub-menu-search')) {
+        // Create the input container
+        inputContainer = document.createElement('div');
+        inputContainer.classList.add('sub-menu-search');
+        
+        // Create the input field
+        const inputField = document.createElement('input');
+        inputField.type = 'text';
+        inputField.placeholder = 'Search...';
+        inputField.classList.add('search-field');
+        
+        // Append input field to the container
+        inputContainer.appendChild(inputField);
+        
+        // Append the container to the search container in the DOM
+        searchContainer.appendChild(inputContainer);
+    }
+});
+
+// Add event listener to the document to close the search input when clicked outside
+document.addEventListener('click', function(event) {
+    // If the click happens outside the search button or inputContainer, hide it
+    if (inputContainer && !searchButton.contains(event.target) && !inputContainer.contains(event.target)) {
+        inputContainer.remove();
+    }
+});
+
+
 document.addEventListener("DOMContentLoaded", function() {
     const switcherButton = document.getElementById("switcher");
     const contentInfoSlider = document.querySelector(".content-info-slider");
